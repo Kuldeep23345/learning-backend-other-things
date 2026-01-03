8c341ca7e1512c045cf3930f083d14d219c91734
Kuldeep <kunalrajput67718@gmail.com>
2026-01-03 22:59:56 +0530

    learning socketio

--- socket.io/index.js
+++ socket.io/index.js
@@ -1,5 +1,26 @@
 const express = require("express");
 const http = require("http");
+const {Server} = require('socket.io')
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server)
+
+app.use(express.static("./public"));
+
+//socket.io
+
+io.on("connection",(socket)=>{
+    socket.on('user-message',(message)=>{
+      io.emit('message',message)
+    })
+
+})
+
+
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => console.log("server started at 9000"));
